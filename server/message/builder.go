2d458833f9bb0dc4ef70d875df6fa0b1a5505ff1
emersion <contact@emersion.fr>
2015-09-25 18:20:03 +0200

    Adds message/builder.send()

--- server/message/builder.go
+++ server/message/builder.go
@@ -5,6 +5,10 @@ import(
 	"encoding/binary"
 )
 
+func send(writer io.Writer, data interface{}) error {
+	return binary.Write(writer, binary.BigEndian, data)
+}
+
 func SendPingResp(writer io.Writer) {
-	binary.Write(writer, binary.BigEndian, GetRespType(Ping))
+	send(writer, GetRespType(Ping))
 }
