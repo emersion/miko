c04dde6b75db0d21f82ff082f134e0c0878209a9
emersion <contact@emersion.fr>
2015-10-11 11:38:35 +0200

    First tests with go browser client

--- server/browser/client.go
+++ server/browser/client.go
@@ -3,21 +3,23 @@ package main
 import(
 	//"github.com/gopherjs/gopherjs/js"
 	"github.com/gopherjs/websocket"
+
+	"git.emersion.fr/saucisse-royale/miko/message"
 )
 
 func main() {
-	c, err := websocket.Dial("ws://localhost/socket")
+	c, err := websocket.Dial("ws://localhost:9998/socket")
 	if err != nil {
 		panic("Dial: " + err.Error())
 	}
 
-	_, err = c.Write([]byte("Hello!"))
+	err = message.SendPing(c)
 	if err != nil {
 		panic("Write: " + err.Error())
 	}
 
-	err = c.Close()
+	/*err = c.Close()
 	if err != nil {
 		panic("Close: " + err.Error())
-	}
+	}*/
 }
--- server/browser/server.go
+++ server/browser/server.go
@@ -1,17 +1,45 @@
 package main
 
 import (
-	"io"
+	"encoding/binary"
 	"net/http"
+	"log"
 	"golang.org/x/net/websocket"
+
+	"../auth"
+	"../message"
+	"../terrain"
 )
 
-func EchoServer(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+func WsServer(ws *websocket.Conn) {
+	clientIO := &message.IO{
+		Reader: ws,
+		Writer: ws,
+		BroadcastWriter: ws,
+		Id: 0,
+	}
+
+	var msg_type message.Type
+	for {
+		err := binary.Read(ws, binary.BigEndian, &msg_type)
+		if err != nil {
+			ws.Close()
+			log.Println("binary.Read failed:", err)
+			return
+		}
+		message.Handle(msg_type, clientIO)
+	}
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(EchoServer))
+	ctx := &message.Context{
+		Auth: auth.NewService(),
+		Terrain: terrain.New(),
+	}
+	message.SetContext(ctx)
+
+	http.Handle("/socket", websocket.Handler(WsServer))
+	http.Handle("/", http.FileServer(http.Dir("public")))
 	err := http.ListenAndServe(":9998", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
