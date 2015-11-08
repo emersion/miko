f4c3342248e9609526f8feb374a280ba77a9c332
emersion <contact@emersion.fr>
2015-11-08 20:34:51 +0100

    Fixes auth.Service.ToAbsoluteTick(), adds more tests

--- server/clock/service.go
+++ server/clock/service.go
@@ -2,6 +2,7 @@ package clock
 
 import (
 	"git.emersion.fr/saucisse-royale/miko.git/server/message"
+	"log"
 	"time"
 )
 
@@ -23,12 +24,20 @@ func (s *Service) GetRelativeTick() message.Tick {
 	return s.ToRelativeTick(s.ticks)
 }
 
-func (s *Service) ToRelativeTick(t uint64) message.Tick {
-	return message.Tick(t)
+func (s *Service) ToRelativeTick(at uint64) message.Tick {
+	return message.Tick(at)
 }
 
-func (s *Service) ToAbsoluteTick(mt message.Tick) uint64 {
-	return uint64(mt) + uint64(s.ticks/(message.MaxTick+1))
+func (s *Service) ToAbsoluteTick(rt message.Tick) uint64 {
+	current := s.GetRelativeTick()
+
+	at := uint64(rt) + uint64(s.ticks-s.ticks%message.MaxTick)
+	log.Println(rt, current)
+	if current < rt {
+		at -= uint64(message.MaxTick)
+	}
+
+	return at
 }
 
 func NewService() *Service {
