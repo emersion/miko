94bd95209d052d3f0e5362a5395f652667f297ea
emersion <contact@emersion.fr>
2015-11-08 19:35:34 +0100

    Improves tests error messages

--- server/auth/service_test.go
+++ server/auth/service_test.go
@@ -16,38 +16,38 @@ func TestService(t *testing.T) {
 
 	loginCode := s.Login(0, username, password)
 	if loginCode != message.LoginResponseCodes["unknown_pseudo"] {
-		t.FailNow()
+		t.Error("Bad response code after wrong login attempt:", loginCode)
 	}
 
 	registerCode := s.Register(0, username, password)
 	if registerCode != message.RegisterResponseCodes["ok"] {
-		t.FailNow()
+		t.Error("Bad register response code:", registerCode)
 	}
 
 	registerCode = s.Register(0, username, "otherpass")
 	if registerCode != message.RegisterResponseCodes["used_pseudo"] {
-		t.FailNow()
+		t.Error("Bad response code after wrong register attempt:", registerCode)
 	}
 
 	loginCode = s.Login(0, username, "otherpass")
 	if loginCode != message.LoginResponseCodes["wrong_password"] {
-		t.FailNow()
+		t.Error("Bad response code after wrong login attempt:", loginCode)
 	}
 
 	loginCode = s.Login(0, username, password)
 	if loginCode != message.LoginResponseCodes["ok"] {
-		t.FailNow()
+		t.Error("Bad login response code:", loginCode)
 	}
 
 	session := s.GetSession(0)
 	if session == nil {
-		t.FailNow()
+		t.Error("Could not get session after login")
 	}
 
 	s.Logout(0)
 
 	session = s.GetSession(0)
 	if session != nil {
-		t.FailNow()
+		t.Error("Session not flushed after logout")
 	}
 }
--- server/clock/service_test.go
+++ server/clock/service_test.go
@@ -19,10 +19,10 @@ func TestService(t *testing.T) {
 	s.Tick()
 
 	if s.GetAbsoluteTick() != 1 {
-		t.Error("Absolute tick not incremented")
+		t.Error("Absolute tick not incremented, should be 1 but is", s.GetAbsoluteTick())
 	}
 	if s.GetRelativeTick() != 1 {
-		t.Error("Relative tick not incremented")
+		t.Error("Relative tick not incremented, should be 1 but is", s.GetRelativeTick())
 	}
 
 	for i := 0; i < message.MaxTick; i++ {
@@ -30,9 +30,9 @@ func TestService(t *testing.T) {
 	}
 
 	if s.GetAbsoluteTick() != 1+message.MaxTick {
-		t.Error("Invalid absolute tick after message.MaxTick ticks")
+		t.Error("Invalid absolute tick after message.MaxTick ticks, should be", 1+message.MaxTick, "but is", s.GetAbsoluteTick())
 	}
 	if s.GetRelativeTick() != 1 {
-		t.Error("Invalid relative tick after message.MaxTick ticks")
+		t.Error("Invalid relative tick after message.MaxTick ticks, should be ", 1, "but is", s.GetRelativeTick())
 	}
 }
