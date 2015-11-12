f772ae0d858f5f12521bed2c1d1fcb16f2fb6e2c
emersion <contact@emersion.fr>
2015-11-12 19:39:04 +0100

    Fixes browser/server for new engine

--- server/browser/server.go
+++ server/browser/server.go
@@ -3,30 +3,21 @@
 package main
 
 import (
-	"git.emersion.fr/saucisse-royale/miko.git/server/auth"
 	"git.emersion.fr/saucisse-royale/miko.git/server/browser/server"
-	"git.emersion.fr/saucisse-royale/miko.git/server/clock"
 	"git.emersion.fr/saucisse-royale/miko.git/server/engine"
-	"git.emersion.fr/saucisse-royale/miko.git/server/entity"
-	"git.emersion.fr/saucisse-royale/miko.git/server/message"
-	"git.emersion.fr/saucisse-royale/miko.git/server/terrain"
 )
 
 func main() {
 	address := ":9998"
 
-	trn := terrain.New()
-	trn.Generate()
+	e := engine.New()
+	ctx := e.Context()
 
-	ctx := message.NewServerContext()
-	ctx.Auth = auth.NewService()
-	ctx.Entity = entity.NewService()
-	ctx.Terrain = trn
-	ctx.Clock = clock.NewService()
+	// Generate a new terrain
+	ctx.Terrain.Generate()
 
 	srv := server.New(address, ctx)
 	go srv.Listen()
 
-	e := engine.New(ctx)
 	e.Start()
 }
