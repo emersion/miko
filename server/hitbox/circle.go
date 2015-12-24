1561c39558e8b0cb08ee3c270d4188dc4548268a
emersion <contact@emersion.fr>
2015-12-24 16:09:13 +0100

    Adds Hitbox.intersects() for circle-circle, rectangle-rectangle and circle-rectangle

--- server/hitbox/circle.go
+++ server/hitbox/circle.go
@@ -1,6 +1,7 @@
 package hitbox
 
 import (
+	"errors"
 	"git.emersion.fr/saucisse-royale/miko.git/server/terrain"
 )
 
@@ -13,6 +14,17 @@ func (hb *Circle) Contour() []*terrain.Position {
 	contour := []*terrain.Position{}
 	// TODO
 	return contour
+}
+
+func (hb *Circle) intersects(other Hitbox) (intersects bool, err error) {
+	switch o := other.(type) {
+	case *Circle:
+		d := dist(hb.center, o.center)
+		intersects = (d <= hb.radius+o.radius)
+	default:
+		err = errors.New("Unsupported hitbox")
+	}
+	return
 }
 
 func NewCircle(center *terrain.Position, radius float64) *Circle {
--- server/hitbox/rectangle.go
+++ server/hitbox/rectangle.go
@@ -1,6 +1,9 @@
 package hitbox
 
 import (
+	"errors"
+	"math"
+
 	"git.emersion.fr/saucisse-royale/miko.git/server/terrain"
 )
 
@@ -31,6 +34,34 @@ func (hb *Rectangle) Contour() []*terrain.Position {
 		bounds[1],
 		&terrain.Position{bounds[0].X, bounds[1].Y},
 	}
+}
+
+func (hb *Rectangle) intersects(other Hitbox) (intersects bool, err error) {
+	switch o := other.(type) {
+	case *Rectangle:
+		b1 := hb.bounds()
+		b2 := o.bounds()
+		intersects = (b1[0].X <= b2[1].X && b1[1].X >= b2[0].X &&
+			b1[0].Y <= b2[1].Y && b1[1].Y >= b2[0].Y)
+	case *Circle:
+		// See http://stackoverflow.com/a/402010
+		bounds := hb.bounds()
+
+		dx := math.Abs(o.center.X - bounds[0].X)
+		dy := math.Abs(o.center.Y - bounds[0].Y)
+
+		if dx > hb.width/2+o.radius || dy > hb.height/2+o.radius {
+			intersects = false
+		} else if dx <= hb.width/2 || dy <= hb.height/2 {
+			intersects = true
+		} else {
+			dc := math.Pow(dx-hb.width/2, 2) + math.Pow(dy-hb.height/2, 2)
+			intersects = (dc <= math.Pow(o.radius, 2))
+		}
+	default:
+		err = errors.New("Unsupported hitbox")
+	}
+	return
 }
 
 func NewRectangle(center *terrain.Position, width, height float64) *Rectangle {
