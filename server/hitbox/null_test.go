1f3d1089b4bec385137d7e7c77a640e9415e0f9d
emersion <contact@emersion.fr>
2015-12-26 14:01:35 +0100

    Adds tests for hitbox/Null

---
+++ server/hitbox/null_test.go
@@ -0,0 +1,16 @@
+package hitbox_test
+
+import (
+	"testing"
+
+	"git.emersion.fr/saucisse-royale/miko.git/server/hitbox"
+)
+
+func TestNull(t *testing.T) {
+	hb := hitbox.NewNull()
+
+	contour := hb.Contour()
+	if len(contour) != 0 {
+		t.Fatal("Null hitbox contour must be empty")
+	}
+}
