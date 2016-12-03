2350b5671ba8abef06037ba3d05655c05a290e4e
delthas <delthas@saucisseroyale.fr.cr>
2016-12-03 01:45:54 +0100

    adds basic support for v10 + reformat

--- client/src/main/java/cr/fr/saucisseroyale/miko/engine/Hitbox.java
+++ client/src/main/java/cr/fr/saucisseroyale/miko/engine/Hitbox.java
@@ -1,104 +1,16 @@
 package cr.fr.saucisseroyale.miko.engine;
 
-import cr.fr.saucisseroyale.miko.util.Pair;
+import cr.fr.saucisseroyale.miko.util.Pair.DoubleFloat;
 import cr.fr.saucisseroyale.miko.util.SingleElementIterable;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
 /**
  * Une hitbox d'entité.
- *
  */
 public abstract class Hitbox {
-
-  private static class Null extends Hitbox {
-
-    @Override
-    public Iterable<Pair.DoubleFloat> getKeyPoints(float deltaX, float deltaY) {
-      return Collections.emptyList();
-    }
-  }
-
-  private static class Circle extends Hitbox {
-
-    private final float radius;
-    private final List<Pair.DoubleFloat> keyPoints;
-
-    public Circle(float radius) {
-      this.radius = radius;
-      // for now approximate circle to ♦
-      // TODO might be optimized to only 4 points
-      List<Pair.DoubleFloat> pairList = new ArrayList<>(5);
-      pairList.add(new Pair.DoubleFloat(0f, 0f));
-      pairList.add(new Pair.DoubleFloat(-radius, 0f));
-      pairList.add(new Pair.DoubleFloat(radius, 0f));
-      pairList.add(new Pair.DoubleFloat(0f, -radius));
-      pairList.add(new Pair.DoubleFloat(0f, radius));
-      keyPoints = Collections.unmodifiableList(pairList);
-    }
-
-    @Override
-    public Iterable<Pair.DoubleFloat> getKeyPoints(float deltaX, float deltaY) {
-      return keyPoints;
-    }
-  }
-
-  private static class Rectangle extends Hitbox {
-
-    private final float widthHalf;
-    private final float heightHalf;
-    private final List<Pair.DoubleFloat> keyPoints;
-
-    public Rectangle(float width, float height) {
-      widthHalf = width / 2;
-      heightHalf = height / 2;
-      List<Pair.DoubleFloat> pairs = new ArrayList<>(5);
-      pairs.add(new Pair.DoubleFloat(0f, 0f));
-      pairs.add(new Pair.DoubleFloat(widthHalf, heightHalf));
-      pairs.add(new Pair.DoubleFloat(widthHalf, -heightHalf));
-      pairs.add(new Pair.DoubleFloat(-widthHalf, heightHalf));
-      pairs.add(new Pair.DoubleFloat(-widthHalf, -heightHalf));
-      keyPoints = Collections.unmodifiableList(pairs);
-    }
-
-    @Override
-    public Iterable<Pair.DoubleFloat> getKeyPoints(float deltaX, float deltaY) {
-      if (deltaX == 0) {
-        if (deltaY > 0) {
-          return new SingleElementIterable<>(new Pair.DoubleFloat(0f, heightHalf));
-        } else if (deltaY < 0) {
-          return new SingleElementIterable<>(new Pair.DoubleFloat(0f, -heightHalf));
-        } else {
-          return Collections.emptyList();
-        }
-      }
-      if (deltaY == 0) {
-        if (deltaX > 0) {
-          return new SingleElementIterable<>(new Pair.DoubleFloat(widthHalf, 0f));
-        } else {
-          return new SingleElementIterable<>(new Pair.DoubleFloat(-widthHalf, 0f));
-        }
-      }
-      int excludeIndex;
-      if (deltaX > 0) {
-        excludeIndex = deltaY > 0 ? 1 : 2;
-      } else {
-        excludeIndex = deltaY > 0 ? 3 : 4;
-      }
-      List<Pair.DoubleFloat> points = new ArrayList<>(4);
-      for (int i = 0, n = keyPoints.size(); i < n; i++) {
-        if (i != excludeIndex) {
-          points.add(keyPoints.get(i));
-        }
-      }
-      return points;
-    }
-
-  }
-
   /**
    * Renvoit true si les deux hitbox, placées respectivement aux coordoonées spécifiées,
    * s'intersectionnent ou sont contenues l'une dans l'autre. Cette fonction est symétrique.
@@ -109,7 +21,7 @@ public abstract class Hitbox {
    * @param m2 La position du centre de la deuxième hitbox.
    * @return true si les hitbox placées aux coordonnées spécifiées s'intersectionnent.
    */
-  public final static boolean collide(Hitbox h1, MapPoint m1, Hitbox h2, MapPoint m2) {
+  public static boolean collide(Hitbox h1, MapPoint m1, Hitbox h2, MapPoint m2) {
     if (h1 instanceof Null || h2 instanceof Null) {
       return false;
     }
@@ -119,7 +31,7 @@ public abstract class Hitbox {
       } else if (h2 instanceof Circle) {
         return rectCircleCollide((Rectangle) h1, m1, (Circle) h2, m2);
       } else {
-        throw new IllegalArgumentException("Unknown hitbox type for h2 : " + h2.toString());
+        throw new IllegalArgumentException("Unknown hitbox type for h2 : " + h2);
       }
     } else if (h1 instanceof Circle) {
       if (h2 instanceof Rectangle) {
@@ -127,25 +39,12 @@ public abstract class Hitbox {
       } else if (h2 instanceof Circle) {
         return circleCollide((Circle) h1, m1, (Circle) h2, m2);
       } else {
-        throw new IllegalArgumentException("Unknown hitbox type for h2 : " + h2.toString());
+        throw new IllegalArgumentException("Unknown hitbox type for h2 : " + h2);
       }
     } else {
-      throw new IllegalArgumentException("Unknown hitbox type for h1 : " + h1.toString());
+      throw new IllegalArgumentException("Unknown hitbox type for h1 : " + h1);
     }
   }
-
-  /**
-   * Retourne les points symbolisant le "contour utile" lors d'une translation de (deltaX;deltaY).
-   * Lors de la translation, on vérifiera les collisions de ce points avec le terrain. C'est une
-   * approximation du contour réel, qui dépend de la direction (et éventuellement de la norme) de la
-   * translation.
-   *
-   * @param deltaX La différence d'abscisses de la translation.
-   * @param deltaY La différence d'ordonnées de la translation.
-   * @return Les points du contour utile, en paires de décalage en x et y par rapport au centre de
-   *         la hitbox/l'entité.
-   */
-  public abstract Iterable<Pair.DoubleFloat> getKeyPoints(float deltaX, float deltaY);
 
   public static Hitbox newCircleHitbox(float radius) {
     return new Circle(radius);
@@ -195,10 +94,100 @@ public abstract class Hitbox {
     if (upLeft1.getX() > downRight2.getX() || upLeft2.getX() > downRight1.getX()) {
       return false;
     }
-    if (upLeft1.getY() < downRight2.getY() || upLeft2.getY() < downRight1.getY()) {
-      return false;
-    }
-    return true;
+    return !(upLeft1.getY() < downRight2.getY() || upLeft2.getY() < downRight1.getY());
   }
 
+  /**
+   * Retourne les points symbolisant le "contour utile" lors d'une translation de (deltaX;deltaY).
+   * Lors de la translation, on vérifiera les collisions de ce points avec le terrain. C'est une
+   * approximation du contour réel, qui dépend de la direction (et éventuellement de la norme) de la
+   * translation.
+   *
+   * @param deltaX La différence d'abscisses de la translation.
+   * @param deltaY La différence d'ordonnées de la translation.
+   * @return Les points du contour utile, en paires de décalage en x et y par rapport au centre de
+   * la hitbox/l'entité.
+   */
+  public abstract Iterable<DoubleFloat> getKeyPoints(float deltaX, float deltaY);
+
+  private static class Null extends Hitbox {
+    @Override
+    public Iterable<DoubleFloat> getKeyPoints(float deltaX, float deltaY) {
+      return Collections.emptyList();
+    }
+  }
+
+  private static class Circle extends Hitbox {
+    private final float radius;
+    private final List<DoubleFloat> keyPoints;
+
+    public Circle(float radius) {
+      this.radius = radius;
+      // for now approximate circle to ♦
+      // TODO might be optimized to only 4 points
+      List<DoubleFloat> pairList = new ArrayList<>(5);
+      pairList.add(new DoubleFloat(0f, 0f));
+      pairList.add(new DoubleFloat(-radius, 0f));
+      pairList.add(new DoubleFloat(radius, 0f));
+      pairList.add(new DoubleFloat(0f, -radius));
+      pairList.add(new DoubleFloat(0f, radius));
+      keyPoints = Collections.unmodifiableList(pairList);
+    }
+
+    @Override
+    public Iterable<DoubleFloat> getKeyPoints(float deltaX, float deltaY) {
+      return keyPoints;
+    }
+  }
+
+  private static class Rectangle extends Hitbox {
+    private final float widthHalf;
+    private final float heightHalf;
+    private final List<DoubleFloat> keyPoints;
+
+    public Rectangle(float width, float height) {
+      widthHalf = width / 2;
+      heightHalf = height / 2;
+      List<DoubleFloat> pairs = new ArrayList<>(5);
+      pairs.add(new DoubleFloat(0f, 0f));
+      pairs.add(new DoubleFloat(widthHalf, heightHalf));
+      pairs.add(new DoubleFloat(widthHalf, -heightHalf));
+      pairs.add(new DoubleFloat(-widthHalf, heightHalf));
+      pairs.add(new DoubleFloat(-widthHalf, -heightHalf));
+      keyPoints = Collections.unmodifiableList(pairs);
+    }
+
+    @Override
+    public Iterable<DoubleFloat> getKeyPoints(float deltaX, float deltaY) {
+      if (deltaX == 0) {
+        if (deltaY > 0) {
+          return new SingleElementIterable<>(new DoubleFloat(0f, heightHalf));
+        } else if (deltaY < 0) {
+          return new SingleElementIterable<>(new DoubleFloat(0f, -heightHalf));
+        } else {
+          return Collections.emptyList();
+        }
+      }
+      if (deltaY == 0) {
+        if (deltaX > 0) {
+          return new SingleElementIterable<>(new DoubleFloat(widthHalf, 0f));
+        } else {
+          return new SingleElementIterable<>(new DoubleFloat(-widthHalf, 0f));
+        }
+      }
+      int excludeIndex;
+      if (deltaX > 0) {
+        excludeIndex = deltaY > 0 ? 1 : 2;
+      } else {
+        excludeIndex = deltaY > 0 ? 3 : 4;
+      }
+      List<DoubleFloat> points = new ArrayList<>(4);
+      for (int i = 0, n = keyPoints.size(); i < n; i++) {
+        if (i != excludeIndex) {
+          points.add(keyPoints.get(i));
+        }
+      }
+      return points;
+    }
+  }
 }
